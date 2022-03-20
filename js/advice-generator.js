49d3f7426ba50ac2c05fe95501eb883f84df76a3
Jacqueline dos Santos Braga <jaquelinesantosbraga@gmail.com>
2022-03-19 21:02:16 -0300

    desafio concluído

---
+++ js/advice-generator.js
@@ -0,0 +1,15 @@
+const adviceId = document.getElementsByClassName("title-id")[0];
+const adviceText = document.getElementsByClassName("adviceApp-text")[0];
+const adviceDice = document.getElementsByClassName("adviceApp-dice")[0];
+
+const getAdvice = () => {
+    fetch("https://api.adviceslip.com/advice").then((response) =>
+      response.json().then((advice) => {
+         adviceId.textContent = advice.slip.id;
+         adviceText.textContent = advice.slip.advice
+      })
+    );
+}
+
+adviceDice.addEventListener("click", getAdvice);
+window.onload = getAdvice;
